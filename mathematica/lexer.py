3b06054f9839efbdb27497a632dc59515cdc5a33
R. Bernstein <rocky@users.noreply.github.com>
2021-03-01 23:06:07 -0500

    Remove FutureWarning: Possible nested set at ... (#9)
    Fixes #8

--- mathematica/lexer.py
+++ mathematica/lexer.py
@@ -12,7 +12,7 @@ import mathematica.builtins as mma
 
 class Regex:
     IDENTIFIER = r'[a-zA-Z\$][a-zA-Z0-9\$]*'
-    NAMED_CHARACTER = r'\\[{identifier}]'.format(identifier=IDENTIFIER)
+    NAMED_CHARACTER = r'\\\[{identifier}]'.format(identifier=IDENTIFIER)
     SYMBOLS = (r'[`]?({identifier}|{named_character})(`({identifier}|{named_character}))*[`]?'
                .format(identifier=IDENTIFIER, named_character=NAMED_CHARACTER))
     INTEGER = r'[0-9]+'
